f556209e48006879e82be5f2a577afc4cccd994b
Rafael Roque <rafael.roque@sendoso.com>
2025-02-06 18:00:26 -0300

    STORY-002: Authentication and security

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\LoginRequest;
+use App\Services\AuthService;
+use App\Traits\ApiResponses;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    use ApiResponses;
+
+    public function __construct(
+        private readonly AuthService $authService)
+    {
+    }
+
+    /**
+     * @param RegisterRequest $request
+     * @return JsonResponse
+     */
+    public function register(RegisterRequest $request) : JsonResponse
+    {
+        $user = $this->authService->register($request->all());
+
+        return $this->success('User registered successfully', 201, $user);
+    }
+
+    /**
+     * @param LoginRequest $request
+     * @return JsonResponse
+     */
+    public function login(LoginRequest $request) : JsonResponse
+    {
+        try {
+            $token = $this->authService->login($request->all());
+            return $this->success('Login successfull', 200, ['access_token' => $token, 'token_type' => 'Bearer']);
+        } catch (ValidationException $e) {
+            return $this->error($e->getMessage(), 401);
+        }
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function logout(Request $request): JsonResponse
+    {
+        $this->authService->logout($request->user());
+        return $this->success('Logged out successfully');
+    }
+}
---
+++ tests/Feature/AuthControllerTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AuthControllerTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    public function test_register()
+    {
+        $response = $this->postJson('/api/register', [
+            'name' => $this->faker->name,
+            'email' => $this->faker->safeEmail,
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ]);
+
+        $response->assertStatus(201)
+            ->assertJson(['message' => 'User registered successfully']);
+    }
+
+    public function test_login()
+    {
+        $user = User::factory()->create([
+            'password' => bcrypt($password = 'password'),
+        ]);
+
+        $response = $this->postJson('/api/login', [
+            'email' => $user->email,
+            'password' => $password,
+        ]);
+
+        $response->assertStatus(200)
+            ->assertJsonStructure(['message', 'data' => ['access_token', 'token_type']]);
+    }
+
+    public function test_logout()
+    {
+        $user = User::factory()->create();
+        $token = $user->createToken('TestToken')->plainTextToken;
+
+        $response = $this->withHeader('Authorization', 'Bearer ' . $token)
+            ->postJson('/api/logout');
+
+        $response->assertStatus(200)
+            ->assertJson(['message' => 'Logged out successfully']);
+    }
+}
